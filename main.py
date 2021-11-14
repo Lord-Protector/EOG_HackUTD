c3b89f3a6fb7ffb47f4d09e0e60f3f7253ff97f9
malcolm7633 <94253924+malcolm7633@users.noreply.github.com>
2021-11-13 19:56:02 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,15 @@ def on_error(wsapp,message):
 def on_message(wsapp,message):
     print(message)
     d=eval(message)
-    a=str(d["flowRateIn"]/len(d["operations"]))
-    output="["
-    for i in range(0,len(d["operations"])):
-        output=output+"{\"operationId\":\""+d["operations"][i]["id"]+"\",\"flowRate\":"+a+"},"
-    output=output[:-1]
-    output=output+"]"
-    print(output)
-    wsapp.send(output)
+    if d["type"]=="CURRENT_STATE":
+        a=str(d["flowRateIn"]/len(d["operations"]))
+        output="["
+        for i in range(0,len(d["operations"])):
+            output=output+"{\"operationId\":\""+d["operations"][i]["id"]+"\",\"flowRate\":"+a+"},"
+        output=output[:-1]
+        output=output+"]"
+        print(output)
+        wsapp.send(output)
 
 wsapp=websocket.WebSocketApp("wss://2021-utd-hackathon.azurewebsites.net",on_message=on_message, on_error=on_error)
 wsapp.run_forever()
