94253bdeba740ba4324aa78081801e9fd34e18a8
Lord-Protector <ethanc199@yahoo.com>
2021-11-14 02:37:35 -0600

    Algorithm Update

--- main.py
+++ main.py
@@ -12,7 +12,12 @@ def on_message(wsapp, message):
         Stores data and sends back the results of the optimization algorithm. """
 
     # stores the data as a dictionary
-    data = ast.literal_eval(message)
+    try:
+        data = ast.literal_eval(message)
+    except:
+        print("Invalid data type.")
+        print(f"{message}\n\n")
+        return 0
 
     # tests whether the message sent is the data from the operations, or the results of the flow allocation
     if data["type"] == "CURRENT_STATE":
@@ -23,7 +28,8 @@ def on_message(wsapp, message):
         # creates the suitable framework for the flow allocation
         output = "["
         for i in range(0, len(data["operations"])):
-            output = output + "{\"operationId\":\"" + data["operations"][i]["id"] + "\",\"flowRate\":" + str(allocate_flow(data)[i]) + "},"
+            output = output + "{\"operationId\":\"" + data["operations"][i]["id"] + "\",\"flowRate\":" + str(
+                allocate_flow(data)[i]) + "},"
         output = output[:-1] + "]"
 
         # prints and sends the flow allocation
@@ -44,48 +50,54 @@ def allocate_flow(data):
         points_row = []
         names.append(operation["name"])
         for i in range(21):
-           points_row.append(operation["revenueStructure"][i]["dollarsPerDay"])
+            points_row.append(operation["revenueStructure"][i]["dollarsPerDay"])
         points.append(points_row)
     slopes = []
     for row in points:
         slopes_row = []
-        for i in range(1,21):
-            dy = row[i] - row[i-1]
+        for i in range(1, 21):
+            dy = row[i] - row[i - 1]
             dx = 10000
-            slope = dy/dx
+            slope = dy / dx
             slopes_row.append(slope)
         slopes.append(slopes_row)
-    maxindeces=[]
+    maxindeces = []
     for row in points:
         maxindeces.append(row.index(max(row)))
-    if sum(maxindeces)*10000>flowRateIn:
-        while sum(maxindeces)*10000-flowRateIn>10000:
-            new=[]
+    if sum(maxindeces) * 10000 > flowRateIn:
+        while sum(maxindeces) * 10000 - flowRateIn > 10000:
+            new = []
             for row in range(len(maxindeces)):
-                new.append(max(points[row][0:maxindeces[row]-1]))
-            workingRow=0
-            workingDif=points[0][maxindeces[0]]-new[0]
+                new.append(max(points[row][0:maxindeces[row] - 1]))
+            workingRow = 0
+            workingDif = points[0][maxindeces[0]] - new[0]
             for row in range(len(maxindeces)):
-                dif=points[row][maxindeces[row]]-new[row]
-                if dif<workingDif:
-                    workingRow=row
-                    workingDif=dif
-            maxindeces[workingRow]=points[workingRow].index(new[workingRow])
-        if sum(maxindeces)*10000>flowRateIn:
-            maxesofeach=[]
+                dif = points[row][maxindeces[row]] - new[row]
+                if dif < workingDif:
+                    workingRow = row
+                    workingDif = dif
+            maxindeces[workingRow] = points[workingRow].index(new[workingRow])
+        if sum(maxindeces) * 10000 > flowRateIn:
+            maxesofeach = []
             for row in range(len(points)):
-                newint=max(points[row][0:maxindeces[row]-1])
-                ylimit=points[row][maxindeces[row]]-slopes[row][maxindeces[row]]*(sum(maxindeces)*10000-flowRateIn)
-                maxesofeach.append([max([newint,ylimit]),ylimit>newint])
-            workingRow=maxesofeach.index(max(maxesofeach))
+                newint = max(points[row][0:maxindeces[row] - 1])
+                ylimit = points[row][maxindeces[row]] - slopes[row][maxindeces[row]] * (
+                            sum(maxindeces) * 10000 - flowRateIn)
+                maxesofeach.append([max([newint, ylimit]), ylimit > newint])
+            workingRow = maxesofeach.index(max(maxesofeach))
             if not maxesofeach[workingRow][1]:
-                maxindeces[workingRow]=points[workingRow].index(maxesofeach[workingRow][0])
+                maxindeces[workingRow] = points[workingRow].index(maxesofeach[workingRow][0])
             else:
-                maxindeces[workingRow]=maxindeces[workingRow]-sum(maxindeces)+flowRateIn/10000
+                maxindeces[workingRow] = maxindeces[workingRow] - sum(maxindeces) + flowRateIn / 10000
     for i in range(len(maxindeces)):
-        maxindeces[i]=maxindeces[i]*10000
-    return(maxindeces)
+        maxindeces[i] = maxindeces[i] * 10000
+    return maxindeces
 
 
-wsapp = websocket.WebSocketApp("wss://2021-utd-hackathon.azurewebsites.net", on_message=on_message, on_error=on_error)
+def on_open(wsapp):
+    wsapp.send("{\"setPitCapacity\": 100000}")
+
+
+wsapp = websocket.WebSocketApp("wss://2021-utd-hackathon.azurewebsites.net", on_message=on_message, on_error=on_error,
+                               on_open=on_open)
 wsapp.run_forever()
