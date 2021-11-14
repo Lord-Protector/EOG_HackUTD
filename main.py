a02eb5f9b57697fc9140da21aa9efc0ce39e3ced
malcolm7633 <94253924+malcolm7633@users.noreply.github.com>
2021-11-13 18:43:09 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 import websocket
-
-def on_message(wsapp, message):
+def on_error(wsapp,message):
     print(message)
+def on_message(wsapp,message):
+    print(message)
+    d=eval(message)
+    #print(d["operations"][0]["revenueStructure"][0]["flowPerDay"])
+    a=str(d["flowRateIn"]/len(d["operations"]))
+    output="["
+    for i in range(0,len(d["operations"])):
+        output=output+"{\"operationId\":\""+d["operations"][i]["id"]+"\",\"flowRate\":"+a+"},"
+    output=output[:-1]
+    output=output+"]"
+    print(output)
+    wsapp.send(output)
 
-wsapp = websocket.WebSocketApp("wss://2021-utd-hackathon.azurewebsites.net", on_message=on_message)
+wsapp=websocket.WebSocketApp("wss://2021-utd-hackathon.azurewebsites.net",on_message=on_message, on_error=on_error)
 wsapp.run_forever()
+#wsapp.connect("wss://2021-utd-hackathon.azurewebsites.net",onmessage=onmessage)
+
+
+#print(wsapp.recv())
