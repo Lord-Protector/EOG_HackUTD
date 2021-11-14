c963fdb50a14ee5a1ba8124d48555d1c29cf4735
Lord-Protector <ethanc199@yahoo.com>
2021-11-13 23:30:41 -0600

    Algorithm Update

--- main.py
+++ main.py
@@ -21,16 +21,16 @@ def on_message(wsapp, message):
         print(f"data = {message}")
 
         # uses the average flow as a placeholder for the allocation
-        ave_flow = str(data["flowRateIn"] / len(data["operations"]))
+        # allocated_flows = allocate_flow(data)
 
         # creates the suitable framework for the flow allocation
         output = "["
-        for i in range(0, len(data["operations"])):
-            output = output + "{\"operationId\":\"" + data["operations"][i]["id"] + "\",\"flowRate\":" + ave_flow + "},"
+        for i in range(len(data["operations"])):
+            output = output + "{\"operationId\":\"" + data["operations"][i]["id"] + "\",\"flowRate\":" + str(data["flowRateIn"]/len(data["operations"])) + "},"
         output = output[:-1] + "]"
 
         # prints and sends the flow allocation
-        print(f"ouput = {output}")
+        print(f"output = {output}")
         wsapp.send(output)
     else:
         print(f"response = {message}\n\n")
@@ -39,9 +39,11 @@ def on_message(wsapp, message):
 def allocate_flow(data):
     """ Stores data and runs the optimization algorithm to determine flow allocation. """
 
-    flowRateIn = data["FlowRateIn"]
+    flowRateIn = data["flowRateIn"]
     operations = data["operations"]
     names = []
+
+    # step 1
     points = []
     for operation in operations:
         points_row = []
@@ -49,6 +51,7 @@ def allocate_flow(data):
         points_row.append(operation["revenueStructure"]["dollarsPerDay"])
         points.append(points_row)
 
+    # step 2
     slopes = []
     for row in points:
         slopes_row = []
@@ -58,6 +61,23 @@ def allocate_flow(data):
             slope = dy/dx
             slopes_row.append(slope)
         slopes.append(slopes_row)
+
+
+    # step 3
+    flow_at_maxes = []
+    for row in points:
+        max_index = row.index(max(row))
+        flow_at_maxes.append(max_index * 10000)
+
+    if sum(flow_at_maxes) < flowRateIn:
+        return flow_at_maxes
+    else:
+        slopes_before_maxes = []
+        for slope_row in slopes:
+            slopes_before_maxes.append(slope_row[max_index-1])
+
+        min_slope = min(slopes_before_maxes)
+        min_slope_index = slopes_before_maxes.index(min_slope)
 
 
 
